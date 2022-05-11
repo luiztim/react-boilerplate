ea15bac1e1a2c17b52c68e9ba6d88219e8f2384e
Luiz Tim <35818924+luiztim@users.noreply.github.com>
2022-05-11 12:47:09 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -13,6 +13,12 @@ import {createRoot} from 'react-dom/client';
 import { App } from './App'
 
 const rootElement = document.getElementById('root');
+
+// Se estiver usando TS, declare root adicionando
+// o operador ! ao final...
+// const root = createRoot(rootElement!);
+
+//Caso contrario, pode declarar assim:
 const root = createRoot(rootElement);
 
 root.render(
