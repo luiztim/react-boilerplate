760c5551b41d0b778619432d96ff97c0da6d55a0
Luiz Tim <35818924+luiztim@users.noreply.github.com>
2022-05-11 12:44:02 -0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,23 @@
-import { render } from 'react-dom';
+/* 
+// React v17 - deprecado
+   import { render } from 'react-dom';
+   import { App } from './App'
+   render(< App />, document.getElementById('root'))
+*/
+
+// React v18
+
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+
 import { App } from './App'
 
-render(< App />, document.getElementById('root'))
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
 
